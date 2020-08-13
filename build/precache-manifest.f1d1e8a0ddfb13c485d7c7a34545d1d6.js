e02f07db3179c8eabd43dd12014aeac482f2fc94
JeremAIh <esiebomaj@gmail.com>
2020-08-13 06:40:39 +0100

    removed build from gitignore

---
+++ build/precache-manifest.f1d1e8a0ddfb13c485d7c7a34545d1d6.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e49daa17a28802ed7d6380f6bd998c45",
+    "url": "/index.html"
+  },
+  {
+    "revision": "080d935c77b4c48fe796",
+    "url": "/static/css/2.eea95c86.chunk.css"
+  },
+  {
+    "revision": "cbdb528f8f83fb9dc538",
+    "url": "/static/css/main.457e08d1.chunk.css"
+  },
+  {
+    "revision": "080d935c77b4c48fe796",
+    "url": "/static/js/2.0bc53cf0.chunk.js"
+  },
+  {
+    "revision": "0bd9f34a651ea9c9fbf93d56da6a9d46",
+    "url": "/static/js/2.0bc53cf0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cbdb528f8f83fb9dc538",
+    "url": "/static/js/main.82f39d6f.chunk.js"
+  },
+  {
+    "revision": "72e4b1f2ff4246389bc8",
+    "url": "/static/js/runtime-main.a50afc49.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
